3428cbec673ea6fb8b6b1c1d02ecef859eb5243f
Alexander Craig <gamefreakf1@gmail.com>
2013-08-27 13:23:05 -0400

    Updated test scripts

--- pox_version/ss_multicast_receiver.py
+++ pox_version/ss_multicast_receiver.py
@@ -10,12 +10,13 @@ import binascii
 # route add -net 224.0.0.0/4 h1-eth0
 
 multicast_group = '224.1.1.1'
+source_address = '10.0.0.1'
 multicast_port = 5007
 packets_to_receive = 30
 echo_port = 5008
 
 def main():
-    global multicast_group, multicast_port, packets_to_receive
+    global multicast_group, multicast_port, packets_to_receive, source_address
     
     if len(sys.argv) > 1:
         multicast_group = sys.argv[1]
@@ -26,12 +27,18 @@ def main():
     if len(sys.argv) > 3:
         packets_to_receive = int(sys.argv[3])
     
+    if len(sys.argv) > 4:
+        source_address = sys.argv[4]
+    
     # Setup the socket for receive multicast traffic
     multicast_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     multicast_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     multicast_socket.bind(('', multicast_port))
-    mreq = struct.pack("=4sl", socket.inet_aton(multicast_group), socket.INADDR_ANY)
-    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    #mreq = struct.pack("=4sl", socket.inet_aton(multicast_group), socket.INADDR_ANY)
+    #multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+    mreq = (socket.inet_pton(socket.AF_INET, multicast_group) +
+                   socket.inet_pton(socket.AF_INET, source_address))
+    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_SOURCE_MEMBERSHIP, mreq)
     
     # Setup the socket for sending echo response
     echo_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -49,7 +56,9 @@ def main():
         if recv_packets > packets_to_receive:
             break
     
-    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
+    multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_SOURCE_MEMBERSHIP, mreq)
+    # mreq = struct.pack("=4sl", socket.inet_aton(multicast_group), socket.INADDR_ANY)
+    # multicast_socket.setsockopt(socket.IPPROTO_IP, socket.IP_DROP_MEMBERSHIP, mreq)
     
 if __name__ == '__main__':
     main()
