53ddbbe6f8e1067fef8468d9a1d556f70bf820cc
Alexander Craig <alexander.craig@carleton.ca>
2014-05-20 17:41:40 -0400

    Updated documentation, readded dependency on groupflow_event_tracer

--- pox/pox/misc/benchmark_terminator.py
+++ pox/pox/misc/benchmark_terminator.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+#  Copyright 2014 Alexander Craig
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
 """
-The purpose of this module is to capture a SIGINT signal, and terminate the benchmarking functionality
+The purpose of this module is to capture a SIGINT signal, and cleanly terminate the benchmarking functionality
 of the FlowTracker and GroupFlowEventTracer modules, before rethrowing the signal so that POX can catch it and terminate
 entirely.
 
-| Depends on misc.groupflow_event_tracer and openflow.flow_tracker
-|
-| Created on Nov 4, 2013
-| @author: alexcraig
+This module does not support any command line arguments.
+
+Depends on misc.groupflow_event_tracer, openflow.flow_tracker
+
+Created on Nov 4, 2013
+
+Author: Alexander Craig - alexcraig1@gmail.com
 """
 
 import time
@@ -36,7 +53,10 @@ class BenchmarkTerminator(EventMixin):
         core.call_when_ready(startup, ('openflow', 'openflow_flow_tracker', 'groupflow_event_tracer'))
 
     def terminate_benchmarking(self, signal, frame):
-        """Terminates the FlowTracker and GroupFlowEventTracer modules, and rethrows the caught signal"""
+        """Terminates the FlowTracker and GroupFlowEventTracer modules, and rethrows the caught signal
+        
+        Terimation is handled by calling the termination_handler() function provided by each module.
+        """
         log.info('Terminating flow tracker module.')
         core.openflow_flow_tracker.termination_handler(signal, frame)
         log.info('Terminated flow tracker module.')
@@ -50,4 +70,5 @@ class BenchmarkTerminator(EventMixin):
 
 
 def launch():
+    # Method called by the POX core when launching the module
     core.registerNew(BenchmarkTerminator)
