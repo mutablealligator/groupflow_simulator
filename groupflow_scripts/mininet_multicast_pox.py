d713446ecc40c0308172adb770ba19b699891eb5
Alexander Craig <alexander.craig@carleton.ca>
2014-05-21 13:55:16 -0400

    Adjusted parameters used in test script.

--- groupflow_scripts/mininet_multicast_pox.py
+++ groupflow_scripts/mininet_multicast_pox.py
@@ -28,9 +28,10 @@ def mcastTest(topo, interactive = False, hosts = [], log_file_name = 'test_log.l
     
     # Launch the external controller
     pox_arguments = ['pox.py', 'log', '--file=pox.log,w', 'openflow.discovery',
-            'openflow.flow_tracker', '--query_interval=1', '--link_max_bw=27', '--link_cong_threshold=24', '--avg_smooth_factor=0.65', '--log_peak_usage=True',
+            'openflow.flow_tracker', '--query_interval=1', '--link_max_bw=28.25', '--link_cong_threshold=20', '--avg_smooth_factor=0.65', '--log_peak_usage=True',
             'misc.benchmark_terminator', 'openflow.igmp_manager', 'misc.groupflow_event_tracer',
             'openflow.groupflow', '--util_link_weight=' + str(util_link_weight), '--link_weight_type=' + link_weight_type, '--flow_replacement_mode=' + replacement_mode,
+            '--flow_replacement_interval=7.5',
             'log.level', '--WARNING', '--openflow.flow_tracker=INFO']
     print 'Launching external controller: ' + str(pox_arguments[0])
     print 'Launch arguments:'
