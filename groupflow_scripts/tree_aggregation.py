83d4509733086388946e3ff4f7030788047c053e
Alexander Craig <aaecraig@connect.carleton.ca>
2014-10-15 13:50:30 -0400

    Added function which calculates multicast group aggregations from clustering linkage array.

--- groupflow_scripts/tree_aggregation.py
+++ groupflow_scripts/tree_aggregation.py
@@ -29,11 +29,40 @@ class McastGroup(object):
         print 'Multicast Group #' + str(self.group_index) + ' Receivers: '
         for receiver_id in self.receiver_ids:
             print str(receiver_id)
+            
+def get_group_aggregation(group_indexes, linkage_array, difference_threshold):
+    group_map = {}
+    for group_index in group_indexes:
+        group_map[group_index] = [group_index]
+    
+    next_cluster_index = len(group_indexes)
+    for cluster in linkage_array:
+        if cluster[2] > difference_threshold:
+            break
+        
+        new_cluster_list = []
+        for index in group_map[cluster[0]]:
+            new_cluster_list.append(index)
+        for index in group_map[cluster[1]]:
+            new_cluster_list.append(index)
+        del group_map[cluster[0]]
+        del group_map[cluster[1]]
+        group_map[next_cluster_index] = new_cluster_list
+        next_cluster_index += 1
+    
+    print 'Group Aggregations for Difference Threshold: ' + str(difference_threshold)
+    for cluster_index in group_map:
+        print 'Cluster Index: ' + str(cluster_index)
+        for group_index in group_map[cluster_index]:
+            print str(group_index) + ' ',
+        print ' '
+            
+    return 0
 
 if __name__ == '__main__':
     groups = []
     
-    for i in range(0, 20):
+    for i in range(0, 10):
         groups.append(McastGroup(i))
         groups[i].generate_random_receiver_ids(randint(1,10), 20)
         groups[i].debug_print()
@@ -47,22 +76,35 @@ if __name__ == '__main__':
     #print 'Receivers Array:\n' + str(receivers_array)
     distance_matrix = []
     group_index = 0
+    group_indexes = []
     for group1 in groups:
         distance_matrix.append([])
         for group2 in groups:
             distance_matrix[group_index].append(group1.jaccard_distance(group2))
+        group_indexes.append(group_index)
         group_index += 1
     #print 'Distance Array Len:' + str(len(distance_matrix))
     #print 'Distance Matrix:\n' + str(distance_matrix)
+    print 'Total Num Groups: ' + str(len(group_indexes))
     
     comp_dist_array = ssd.squareform(distance_matrix)
     #print 'Compressed Distance Array Len: ' + str(len(comp_dist_array))
     
     plt.figure(1, figsize=(6, 5))
     z = linkage(comp_dist_array, method='single', metric='jaccard')
-    #print 'Linkage Array:\n' + str(z)
+    print 'Linkage Array:\n' + str(z)
+    print ' '
+    get_group_aggregation(group_indexes, z, 0.7)
     d = dendrogram(z, show_leaf_counts=True)
-    plt.title('Multicast Group Clustering')
+    plt.title('Multicast Group Clustering (Single Linkage)')
+    plt.xlabel('Multicast Group Index')
+    plt.ylabel('Cluster Distance')
+    
+    plt.figure(2, figsize=(6, 5))
+    z = linkage(comp_dist_array, method='complete', metric='jaccard')
+    print 'Linkage Array:\n' + str(z)
+    d = dendrogram(z, show_leaf_counts=True)
+    plt.title('Multicast Group Clustering (Complete Linkage)')
     plt.xlabel('Multicast Group Index')
     plt.ylabel('Cluster Distance')
     plt.show()
